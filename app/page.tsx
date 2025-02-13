66c4b11ab87d7b6756819af3f042abe5d95ba820
dave.avershion <przywara.dave@gmail.com>
2025-02-13 18:55:04 +0100

    Main page styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@ import WalletConnector from "@/components/WalletConnector/WalletConnector";
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col lg:flex-row gap-8 row-start-2 items-center">
+        <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center h-screen p-4 sm:p-8 gap-8 font-[family-name:var(--font-geist-sans)]">
+            <header className="w-full text-center"></header>
+
+            <main className="flex flex-col lg:flex-row items-start justify-center gap-8">
                 <WalletConnector />
                 <TokenList />
             </main>
+            <footer className="w-full text-center"></footer>
         </div>
     );
 }
